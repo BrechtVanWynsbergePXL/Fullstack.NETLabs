da9280f763f22e40a28ad9e403b9eecfd96f0afc
BrechtVanWynsbergePXL <brecht.vanwynsberge@student.pxl.be>
2021-10-19 00:30:57 +0200

    solved all percentage tests

--- KWops/src/Services/DevOps/DevOps.Domain/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace DevOps.Domain
-{
-    public class Class1
-    {
-    }
-}
---
+++ KWops/src/Services/DevOps/DevOps.Domain/Percentage.cs
@@ -0,0 +1,35 @@
+﻿using Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DevOps.Domain
+{
+    public class Percentage : ValueObject<Percentage>
+    {
+        private readonly double _value;
+        public Percentage(double value)
+        {
+            if(value < 0.0 || value > 1.0)
+            {
+                throw new ContractException();
+            }
+            _value = value;
+        }
+
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return _value;
+        }
+
+        public override string ToString()
+        {
+            return Math.Round(_value * 100, 2).ToString().Replace('.', ',') + "%";
+        }
+
+        public static implicit operator string(Percentage value) => value.ToString();
+        public static implicit operator double(Percentage value) => value;
+        public static implicit operator Percentage(double value) => new Percentage(value);
+       
+    }
+}
